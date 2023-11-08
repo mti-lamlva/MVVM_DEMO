de88271592143fcd56171af1a40734eeac97f375
LamLVA <lamlva@techvn.mti.co.jp>
2023-11-08 10:15:53 +0700

    Initial Commit

---
+++ NewsAPIServiceUITests/NewsAPIServiceUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NewsAPIServiceUITestsLaunchTests.swift
+//  NewsAPIServiceUITests
+//
+//  Created by Le Van Anh Lam(LamLVA) on 08/11/2023.
+//
+
+import XCTest
+
+final class NewsAPIServiceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
